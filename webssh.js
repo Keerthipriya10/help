664a23fa3030b916a5b74d0d16bb5f6659c1a0a4
Keerthipriya10 <45307758+Keerthipriya10@users.noreply.github.com>
2023-10-12 21:45:32 +0530

    Create webssh.js

---
+++ webssh.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    sshFormData: {
+      hostname: 'example.com', // Replace with your SSH server's hostname
+      port: '22', // Replace with your SSH server's port
+      username: 'your_username', // Replace with your SSH username
+      password: 'your_password', // Replace with your SSH password
+      term: 'xterm-256color', // Replace with the desired terminal type
+      jsondata: '', // Initialize the JSON data field as an empty string
+    },
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState((prevState) => ({
+      sshFormData: {
+        ...prevState.sshFormData,
+        [name]: value,
+      },
+    }));
+  };
+
+  handleConnect = () => {
+    // Create form data with the SSH connection details and JSON data.
+    const formData = new FormData();
+    const { sshFormData } = this.state;
+
+    Object.entries(sshFormData).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    // Serialize the form data to a query string
+    const queryString = new URLSearchParams(formData).toString();
+
+    // Open a new window and redirect to the WebSSH form with the query string
+    const newWindow = window.open(`http://localhost:8888/?${queryString}`, '_blank');
+    if (newWindow) {
+      newWindow.focus(); // Focus on the new window
+    }
+  };
+
+  render() {
+    const { sshFormData } = this.state;
+
+    return (
+      <div>
+        <div>
+          <label>
+            Hostname:
+            <input type="text" name="hostname" value={sshFormData.hostname} onChange={this.handleInputChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Port:
+            <input type="text" name="port" value={sshFormData.port} onChange={this.handleInputChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Username:
+            <input type="text" name="username" value={sshFormData.username} onChange={this.handleInputChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password:
+            <input type="password" name="password" value={sshFormData.password} onChange={this.handleInputChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Terminal Type:
+            <input type="text" name="term" value={sshFormData.term} onChange={this.handleInputChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            JSON Data:
+            <textarea
+              name="jsondata"
+              value={sshFormData.jsondata}
+              onChange={this.handleInputChange}
+              rows="4"
+              cols="50"
+            ></textarea>
+          </label>
+        </div>
+        <div>
+          <button onClick={this.handleConnect}>Connect to SSH</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
